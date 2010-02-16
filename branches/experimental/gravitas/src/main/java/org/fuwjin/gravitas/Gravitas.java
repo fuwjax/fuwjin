71bbf6e464af3cc906b917a38eabe54f4f8a6739
fuwjax <fuwjax@b91389ce-0c5c-11df-a5af-b38b39d06e06>
2010-02-16 08:33:14 +0000

    almost cleaned up all the tests

--- branches/experimental/gravitas/src/main/java/org/fuwjin/gravitas/Gravitas.java
+++ branches/experimental/gravitas/src/main/java/org/fuwjin/gravitas/Gravitas.java
@@ -1,15 +1,16 @@
 package org.fuwjin.gravitas;
 
+import static com.google.inject.Guice.createInjector;
+import static java.util.ServiceLoader.load;
+
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ServiceLoader;
 
 import org.fuwjin.gravitas.engine.ExecutionEngine;
 import org.fuwjin.gravitas.gesture.EventRouter;
 import org.fuwjin.gravitas.gesture.UserInstructionEventHandler;
 import org.fuwjin.gravitas.root.RootContext;
 
-import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -17,20 +18,20 @@ import com.google.inject.Singleton;
 
 @Singleton
 public class Gravitas{
-   public static void main() throws Exception{
-      main(new String[0]);
+   public static void main(final String... args) throws Exception{
+      startGravitas(load(Module.class));
    }
-   
-   public static void main(final String[] args) throws Exception{
-      ServiceLoader<Module> modules = ServiceLoader.load(Module.class);
+
+   public static Injector startGravitas(Iterable<Module> modules){
       List<Module> list = new LinkedList<Module>();
       list.add(new GravitasModule());
       for(Module module: modules){
          list.add(module);
       }
-      final Injector guice = Guice.createInjector(list);
+      final Injector guice = createInjector(list);
       final Gravitas gravitas = guice.getInstance(Gravitas.class);
       gravitas.start();
+      return guice;
    }
 
    @Inject
