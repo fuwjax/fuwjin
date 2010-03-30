bfd10edd3925d643ea578e2a13d9f74b19f40bd8
fuwjax <fuwjax@b91389ce-0c5c-11df-a5af-b38b39d06e06>
2010-03-30 06:14:03 +0000

    Share project "chrona" into "https://fuwjin.googlecode.com/svn"

---
+++ branches/experimental/chrona/src/main/java/org/fuwjin/chrona/Chrona.java
@@ -0,0 +1,19 @@
+package org.fuwjin.chrona;
+
+public class Chrona{
+   public static void enable(){
+      
+   }
+   
+   public static void disable(){
+      
+   }
+   
+   public static long nanoTime(long time){
+      return 0L;
+   }
+   
+   public static long currentTimeMillis(long time){
+      return 0L;
+   }
+}
