0d63afdd32940e2d5f4bfc8a72df3a7539f70939
fuwjax@gmail.com <fuwjax@gmail.com@b91389ce-0c5c-11df-a5af-b38b39d06e06>
2010-10-05 07:12:30 +0000

    fixed copyright notices

--- trunk/jon/src/main/java/org/fuwjin/jon/ref/ReferenceStorage.java
+++ trunk/jon/src/main/java/org/fuwjin/jon/ref/ReferenceStorage.java
@@ -4,10 +4,10 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *    Michael Doberenz - initial implementation
- *******************************************************************************/
+ *     Michael Doberenz - initial API and implementation
+ ******************************************************************************/
 package org.fuwjin.jon.ref;
 
 import static org.fuwjin.jon.BuilderRegistry.getBuilder;
