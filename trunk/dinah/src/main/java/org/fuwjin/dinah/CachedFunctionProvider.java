848064a093e8479b9f4a91794e3724ae7e3c679e
fuwjax <fuwjax@b91389ce-0c5c-11df-a5af-b38b39d06e06>
2011-05-18 05:11:52 +0000

    moving "instanceof" to "isInstance" making CatalogManager a FunctionProvider

---
+++ trunk/dinah/src/main/java/org/fuwjin/dinah/CachedFunctionProvider.java
@@ -0,0 +1,30 @@
+package org.fuwjin.dinah;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.fuwjin.dinah.function.AbstractFunction;
+
+public class CachedFunctionProvider extends AbstractFunctionProvider {
+   private final AbstractFunctionProvider[] providers;
+   private final Set<String> categories = new HashSet<String>();
+   private final Map<String, AbstractFunction> functions = new HashMap<String, AbstractFunction>();
+
+   public CachedFunctionProvider(final AbstractFunctionProvider... providers) {
+      this.providers = providers;
+   }
+
+   @Override
+   public Map<String, AbstractFunction> getFunctions(final String category) {
+      if(!categories.contains(category)) {
+         for(final AbstractFunctionProvider provider: providers) {
+            for(final AbstractFunction func: provider.getFunctions(category).values()) {
+               add(functions, func);
+            }
+         }
+         categories.add(category);
+      }
+      return functions;
+   }
+}
---
+++ trunk/dinah/src/main/java/org/fuwjin/dinah/function/InstanceMethodFunction.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Michael Doberenz. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Michael Doberenz -
+ * initial API and implementation
+ ******************************************************************************/
+package org.fuwjin.dinah.function;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import org.fuwjin.dinah.Adapter;
+
+/**
+ * Function for reflective method invocation.
+ */
+public class InstanceMethodFunction extends FixedArgsFunction<Method> {
+   private final Object target;
+
+   /**
+    * Creates a new instance.
+    * @param category the function category
+    * @param method the method to invoke
+    * @param type the host object type
+    */
+   public InstanceMethodFunction(final Adapter adapter, final String category, final Method method, final Object target) {
+      super(adapter, method, category + '.' + method.getName(), method.getParameterTypes());
+      this.target = target;
+   }
+
+   @Override
+   protected Object invokeSafe(final Object... args) throws IllegalArgumentException, IllegalAccessException,
+         InvocationTargetException {
+      final Object result = member().invoke(target, args);
+      if(member().getReturnType().equals(void.class)) {
+         return Adapter.UNSET;
+      }
+      return result;
+   }
+}
