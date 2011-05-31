185c29f8b11e270a24257e085316cfc5f46796f4
fuwjax <fuwjax@b91389ce-0c5c-11df-a5af-b38b39d06e06>
2011-05-31 00:10:53 +0000

    initial tweed import, hardly working

---
+++ trunk/tweed/tweed.LD/Adapter/Adapter.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Tweed.LD.Adapter
+{
+	public class Adapter:IAdapter
+	{
+		public Adapter ()
+		{
+		}
+
+		public object Adapt (object value, Type type)
+		{
+			throw new System.NotImplementedException();
+		}
+		
+	}
+}
+
